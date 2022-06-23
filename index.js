cbeab5b73ba2ef6cfe2ed1cdac096f411359b982
Bolumorin <boluwadamilola04@gmail.com>
2022-06-23 15:30:04 +0100

    Zuri JS Task- Biodata

---
+++ index.js
@@ -0,0 +1,5 @@
+alert('Just a little bit about me. Let us go!');
+
+document.write('My name is Boluwatife Somorin.  <br>');
+document.write ('I am from Nigeria.  <br>');
+document.write('And I am 5ft 8inches tall.');
